fdc528acde14be64e0d7b010f0c284e389b6b5db
UniformSA Admin <admin@uniformsa.com>
2025-05-16 09:19:22 +0300

    Update Netlify configuration and build settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: false,
+  output: 'export',
   images: {
     // Making sure our images are optimized
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
@@ -8,16 +8,12 @@ const nextConfig = {
     // Remote patterns for image sources
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
+        protocol: 'https',
+        hostname: 'gregarious-sawine-ed40c7.netlify.app',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'gregarious-sawine-ed40c7.netlify.app',
       }
     ],
     unoptimized: true,
