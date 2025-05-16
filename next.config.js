5c049d8c774858ca6ff8f8a8688dd1697412299c
UniformSA Admin <admin@uniformsa.com>
2025-05-16 12:37:45 +0300

    Fix syntax error in next.config.js

--- next.config.js
+++ next.config.js
@@ -11,8 +11,8 @@ const nextConfig = {
   },
   env: {
     NEXT_PUBLIC_SITE_URL: 'https://gregarious-sawine-ed40c7.netlify.app',
-    NEXT_PUBLIC_DOMAIN: 'gregarious-sawine-ed40c7.netlify.app',
+    NEXT_PUBLIC_DOMAIN: 'gregarious-sawine-ed40c7.netlify.app'
   }
-}
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig; 
